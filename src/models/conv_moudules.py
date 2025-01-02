4bf114718ed0aef9a86fc213bb1eb1bc55c4be40
ByungWanLim <pook0612@naver.com>
2025-01-02 23:50:13 +0900

    UnetResBlock 클래스 추가 및 Bottleneck, CSPBlock의 정규화 함수 변경

--- src/models/conv_moudules.py
+++ src/models/conv_moudules.py
@@ -78,7 +78,7 @@ class Bottleneck(nn.Module):
         super().__init__()
         c_ = int(c2 * e)  
         self.cv1 = get_conv_layer(3, c1, c_, kernel_size=k[0], stride=1, norm=norm_name, act=act_name) # spatial_dims를 3으로 변경
-        self.cv2 = get_conv_layer(3, c_, c2, kernel_size=k[1], stride=1, norm=norm_name, act=act_name, bias=False)
+        self.cv2 = get_conv_layer(3, c_, c2, kernel_size=k[1], stride=1, norm=norm_name, act=act_name)
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
@@ -153,6 +153,8 @@ class CSPBlock(nn.Module):
         x = torch.split(x, (self.split_channels,self.split_channels), dim=1)
         x1 = self.left(x[0])
         x2 = self.right(x[1])
+        # print(f"x1 mean: {x1.mean().item()}, std: {x1.std().item()}")
+        # print(f"x2 mean: {x2.mean().item()}, std: {x2.std().item()}")
         x = torch.cat([x1, x2], dim=1)
         return self.final(x)
     
@@ -213,6 +215,95 @@ class UPCSPBlock(nn.Module):
         out = self.conv_block(out)
         return out
 
+
+class UnetResBlock(nn.Module):
+    """
+    A skip-connection based module that can be used for DynUNet, based on:
+    `Automated Design of Deep Learning Methods for Biomedical Image Segmentation <https://arxiv.org/abs/1904.08128>`_.
+    `nnU-Net: Self-adapting Framework for U-Net-Based Medical Image Segmentation <https://arxiv.org/abs/1809.10486>`_.
+
+    Args:
+        spatial_dims: number of spatial dimensions.
+        in_channels: number of input channels.
+        out_channels: number of output channels.
+        kernel_size: convolution kernel size.
+        stride: convolution stride.
+        norm_name: feature normalization type and arguments.
+        act_name: activation layer type and arguments.
+        dropout: dropout probability.
+
+    """
+
+    def __init__(
+        self,
+        spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Sequence[int] | int,
+        stride: Sequence[int] | int,
+        norm_name: tuple | str,
+        act_name: tuple | str = ("leakyrelu", {"inplace": True, "negative_slope": 0.01}),
+        dropout: tuple | str | float | None = None,
+    ):
+        super().__init__()
+        self.conv1 = get_conv_layer(
+            spatial_dims,
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            dropout=dropout,
+            act=None,
+            norm=None,
+            conv_only=False,
+        )
+        self.conv2 = get_conv_layer(
+            spatial_dims,
+            out_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            dropout=dropout,
+            act=None,
+            norm=None,
+            conv_only=False,
+        )
+        self.lrelu = get_act_layer(name=act_name)
+        self.norm1 = get_norm_layer(name=norm_name, spatial_dims=spatial_dims, channels=out_channels)
+        self.norm2 = get_norm_layer(name=norm_name, spatial_dims=spatial_dims, channels=out_channels)
+        self.downsample = in_channels != out_channels
+        stride_np = np.atleast_1d(stride)
+        if not np.all(stride_np == 1):
+            self.downsample = True
+        if self.downsample:
+            self.conv3 = get_conv_layer(
+                spatial_dims,
+                in_channels,
+                out_channels,
+                kernel_size=1,
+                stride=stride,
+                dropout=dropout,
+                act=None,
+                norm=None,
+                conv_only=False,
+            )
+            self.norm3 = get_norm_layer(name=norm_name, spatial_dims=spatial_dims, channels=out_channels)
+
+    def forward(self, inp):
+        residual = inp
+        out = self.conv1(inp)
+        out = self.norm1(out)
+        out = self.lrelu(out)
+        out = self.conv2(out)
+        out = self.norm2(out)
+        if hasattr(self, "conv3"):
+            residual = self.conv3(residual)
+        if hasattr(self, "norm3"):
+            residual = self.norm3(residual)
+        out += residual
+        out = self.lrelu(out)
+        return out
+
 if __name__ == '__main__':
     block = CSPBlock(
         spatial_dims=3,
@@ -220,14 +311,25 @@ if __name__ == '__main__':
         out_channels=128,
         kernel_size=3,
         stride=2,
-        norm_name="batch",
+        norm_name="instance",
         act_name=("leakyrelu", {"inplace": True, "negative_slope": 0.01}),
         dropout=None,
-        split_ratio=0.5
+        split_ratio=0.5,
+        n=1
     )
     x = torch.randn(1, 64, 24, 24, 24)
     p = block(x)
     
+    unetresblock = UnetResBlock(
+        spatial_dims=3,
+        in_channels=64,
+        out_channels=128,
+        kernel_size=3,
+        stride=2,
+        norm_name="instance",
+        act_name=("leakyrelu", {"inplace": True, "negative_slope": 0.01}),
+        dropout=None,
+    )
     
     upbolck = UPCSPBlock(
         spatial_dims=3,
@@ -235,10 +337,14 @@ if __name__ == '__main__':
         out_channels=64,
         kernel_size=3,
         upsample_kernel_size=2,
-        norm_name="batch",
-        
+        norm_name="instance",
+        n=1
     )
     print(f"Input shape: {x.shape}")
-    print(f"Output shape: {p.shape}")
+    print(f"CSPBlock Output shape: {p.shape}")
+    print(f"UnetResBlock Output shape: {unetresblock(x).shape}")
     print(f"UP Output shape: {upbolck(p,x).shape}")
+    print(f"CSPBlock mean: {torch.mean(p)}")
+    print(f"UnetResBlock mean: {torch.mean(unetresblock(x))}")
+    print(f"UPCSPBlock mean: {torch.mean(upbolck(p,x))}")
         
