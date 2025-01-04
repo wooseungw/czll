d25caf8c1a5d693bf51ea10b54e797f6989ff943
ByungwanLim <pook0612@naver.com>
2025-01-04 21:44:40 +0900

    SwinCSPUNETR_unet 모델 추가 및 플로프스, 파라미터 출력 개선

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from .swin_transformer import SwinTransformer, MERGING_MODE
 from .conv_moudules import UnetResBlock, CSPBlock
 from .swincspunetr import SwinCSPUNETR
+from .swincspunetr_unet import SwinCSPUNETR_unet
