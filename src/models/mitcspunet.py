d33002b783fc04742fb7996b51f6ca1bea6d3b15
wooseungw <woow070840@gmail.com>
2025-01-08 02:08:46 +0900

    daformerableLKA모델 추가 및 관련 코드 수정, 배치 사이즈 및 메타데이터 업데이트

--- src/models/mitcspunet.py
+++ src/models/mitcspunet.py
@@ -193,3 +193,101 @@ class MiTCSPUnet(nn.Module):
         out = self.out(dec0)
         
         return out
+
+
+class MiTUnet(nn.Module):
+    def __init__(
+        self,
+        *,
+        img_size = 96,
+        feature_size,
+        heads=(1, 1, 2, 5, 8),
+        ff_expansion=(2, 8, 8, 4, 4),
+        reduction_ratio=(16, 8, 4, 2, 1),
+        num_layers=2,
+        channels=1,
+        stage_kernel_stride_pad = ((3, 1, 1), (3, 2, 1), (3, 2, 1), (3, 2, 1), (3, 2, 1)),
+        
+        spatial_dims=3,
+        out_channels=7,
+        norm_name="instance",
+        act_name = ("leakyrelu ", {"inplace": True, "negative_slope": 0.01}),
+        n=2,
+        
+    ):
+        super().__init__()
+        dims = (feature_size, feature_size * 2, feature_size * 4, feature_size * 8, feature_size * 16)
+        depth = len(stage_kernel_stride_pad)
+        heads, ff_expansion, reduction_ratio, num_layers = map(
+            partial(cast_tuple, depth=depth), (heads, ff_expansion, reduction_ratio, num_layers))
+        assert all([*map(lambda t: len(t) == depth, (heads, ff_expansion, reduction_ratio, num_layers))]), \
+            'only four stages are allowed, all keyword arguments must be either a single value or a tuple of 4 values'
+
+        self.mit = MiT3D(  # Use the 3D MiT
+            channels=channels,
+            dims=dims,
+            heads=heads,
+            ff_expansion=ff_expansion,
+            reduction_ratio=reduction_ratio,
+            num_layers=num_layers,
+            stage_kernel_stride_pad=stage_kernel_stride_pad
+        )
+
+        self.decoder4 = UPCSPBlock(
+            spatial_dims=spatial_dims,
+            in_channels=16 * feature_size,
+            out_channels=8 * feature_size,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            n=n
+        )
+
+        self.decoder3 = UPCSPBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size * 8,
+            out_channels=feature_size * 4,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            n=n
+        )
+
+        self.decoder2 = UPCSPBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size * 4,
+            out_channels=feature_size * 2,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            n=n
+        )
+        self.decoder1 = UPCSPBlock(
+            spatial_dims=spatial_dims,
+            in_channels=feature_size * 2,
+            out_channels=feature_size,
+            kernel_size=3,
+            upsample_kernel_size=2,
+            norm_name=norm_name,
+            n=n
+        )
+
+        
+
+        self.out = UnetOutBlock(spatial_dims=spatial_dims, in_channels=feature_size, out_channels=out_channels)
+
+
+    def forward(self, x):
+        # Extract features from MiT3D
+        hidden_states_out = self.mit(x, return_layer_outputs=True)
+        # print(len(hidden_states_out))
+        # for i in range(len(hidden_states_out)):
+        #     print(hidden_states_out[i].shape)
+        x = self.decoder4(hidden_states_out[4], hidden_states_out[3]) # 16 * feature_size -> 8 * feature_size
+        x = self.decoder3(x, hidden_states_out[2]) # 8 * feature_size -> 4 * feature_size
+        x = self.decoder2(x, hidden_states_out[1]) # 4 * feature_size -> 2 * feature_size
+        x = self.decoder1(x, hidden_states_out[0]) # 2 * feature_size -> feature_size
+        x = self.out(x)
+        
+        return x
+
