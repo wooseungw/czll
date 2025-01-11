9ecd98258d9c8b5925027455339dd566b5cd45e3
ByungwanLim <pook0612@naver.com>
2025-01-11 21:47:56 +0900

    UNet 모델 추가 및 불필요한 코드 주석 처리

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,6 +4,9 @@ from .swincspunetr import SwinCSPUNETR
 from .swincspunetr_unet import SwinCSPUNETR_unet
 from .swincspunetr3plus import SwinCSPUNETR3plus
 from .mitcspunet import MiTCSPUnet, MiTUnet
-from .defomer_lka import D_LKA_Net
+from .unet import UNet
 
-from src.models.deformer_lka_blocks import D_LKA_NetEncoder, D_LKA_NetUpBlock
+
+# from .defomer_lka import D_LKA_Net
+
+# from src.models.deformer_lka_blocks import D_LKA_NetEncoder, D_LKA_NetUpBlock
